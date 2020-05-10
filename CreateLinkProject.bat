ccfc604791198ef9f4665b644bb0dc635b6c0d76
Koike.hideki <hdkkik@gmail.com>
2020-05-11 03:56:57 +0900

    fix to check file existing

--- CreateLinkProject.bat
+++ CreateLinkProject.bat
@@ -12,27 +12,35 @@ mkdir %made_path%
 
 
 rem フォルダのシンボリックリンクを作成
-if exist %ProjectName%\.git
-MKLINK /d %made_path%\.git %ProjectName%\.git
 
-if exist %ProjectName%\Assets
-MKLINK /d %made_path%\Assets %ProjectName%\Assets
+if exist %ProjectName%\.git (
+    MKLINK /d %made_path%\.git %ProjectName%\.git
+)
 
-if exist %ProjectName%\Library
-MKLINK /d %made_path%\Library %ProjectName%\Library
+if exist %ProjectName%\Assets (
+    MKLINK /d %made_path%\Assets %ProjectName%\Assets
+)
 
-if exist %ProjectName%\Packages
-MKLINK /d %made_path%\Packages %ProjectName%\Packages
+if exist %ProjectName%\Library (
+    MKLINK /d %made_path%\Library %ProjectName%\Library
+)
 
-if exist %ProjectName%\ProjectSettings
-MKLINK /d %made_path%\ProjectSettings %ProjectName%\ProjectSettings
+if exist %ProjectName%\Packages (
+    MKLINK /d %made_path%\Packages %ProjectName%\Packages
+)
+
+if exist %ProjectName%\ProjectSettings (
+    MKLINK /d %made_path%\ProjectSettings %ProjectName%\ProjectSettings
+)
 
 
 rem ファイルのシンボリックリンクを作成
-if exist %ProjectName%\.editorconfig
-MKLINK %made_path%\.editorconfig %ProjectName%\.editorconfig
+if exist %ProjectName%\.editorconfig (
+    MKLINK %made_path%\.editorconfig %ProjectName%\.editorconfig
+)
 
-if exist %ProjectName%\.gitignore
-MKLINK %made_path%\.gitignore %ProjectName%\.gitignore
+if exist %ProjectName%\.gitignore (
+    MKLINK %made_path%\.gitignore %ProjectName%\.gitignore
+)
 
 pause
